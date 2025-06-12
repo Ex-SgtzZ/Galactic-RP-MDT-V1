c1c116018b71c27e39096e4146f36faa3013071d
James Peterson <joneszayden65@outlook.com>
2025-06-12 18:19:08 -0500

    NEW CLIENT SCRIPT

--- client/client.lua
+++ client/client.lua
@@ -1,65 +1,17 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Open MDT command
-RegisterCommand("mdt", function()
+RegisterCommand(Config.Commands.openMDT, function()
     local Player = QBCore.Functions.GetPlayerData()
+    local hasItem = exports['qb-inventory']:HasItem(Config.MDTItem)
 
-    if not HasMDTItem(Player) then
-        QBCore.Functions.Notify("You don't have an MDT device.", "error")
-        return
+    if not hasItem then
+        return QBCore.Functions.Notify("You need an MDT device to open this.", "error")
     end
 
-    if Player.job.onduty then
-        TriggerServerEvent('mdt:requestConfig')
-    else
-        QBCore.Functions.Notify("You must be on duty to use the MDT.", "error")
-    end
-end)
-
--- Receive config from server and open correct NUI skin
-RegisterNetEvent('mdt:receiveConfig', function(jobConfig)
+    local skin = Config.Jobs[Player.job.name] and Config.Jobs[Player.job.name].skin or "default"
     SetNuiFocus(true, true)
     SendNUIMessage({
-        action = "open",
-        skin = jobConfig.skin,
-        permissions = {
-            canEditPenalCodes = jobConfig.canEditPenalCodes,
-            canManageWarrants = jobConfig.canManageWarrants,
-            canManageCourtCases = jobConfig.canManageCourtCases
-        }
+        action = "openMDT",
+        skin = skin
     })
 end)
-
--- Close MDT from NUI
-RegisterNUICallback("close", function(data, cb)
-    SetNuiFocus(false, false)
-    cb("ok")
-end)
-
--- Admin MDT creation panel
-RegisterCommand("mdtcreate", function()
-    TriggerServerEvent("mdt:adminCheck")
-end)
-
-RegisterNetEvent("mdt:openAdminMDTCreator", function()
-    SetNuiFocus(true, true)
-    SendNUIMessage({ action = "openAdmin" })
-end)
-
--- Send new job creation to server
-RegisterNUICallback("submitNewJob", function(data, cb)
-    TriggerServerEvent("mdt:addJob", data)
-    cb("ok")
-end)
-
--- Simple inventory check
-function HasMDTItem(Player)
-    local hasItem = false
-    for k, v in pairs(Player.items) do
-        if v.name == Config.RequiredItem then
-            hasItem = true
-            break
-        end
-    end
-    return hasItem
-end
